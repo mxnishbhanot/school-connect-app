a57a193f38f0bae22aa9e5043e81c2df49d823f7
Manish Kumar <manish.kumar@commitbit.com>
2025-09-11 01:31:34 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'school-connect-app',
+  webDir: 'www'
+};
+
+export default config;
